c87d8a4222977010f796e52e09404f773288582e
Siva Nadesan <entechlog@gmail.com>
2021-02-07 22:13:47 -0800

    Added DataDog metric sink connector

--- kafka-to-datadog/kafka-connect/scripts/create-datadog-metric-sink.sh
+++ kafka-to-datadog/kafka-connect/scripts/create-datadog-metric-sink.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 curl -i -X PUT -H  "Content-Type:application/json" \
-    http://kafka-connect:8083/connectors/MOCKAROO_DATADOG_SINK/config \
+    http://kafka-connect:8083/connectors/METRIC_DATADOG_SINK/config \
     -d '{
         "connector.class":"io.confluent.connect.datadog.metrics.DatadogMetricsSinkConnector",
         "tasks.max":1,
         "confluent.topic.bootstrap.servers": "broker:9092",
-        "topics":"datadog-sample-events",
+        "topics":"backend-server-capacity",
         "key.converter": "org.apache.kafka.connect.storage.StringConverter",
         "key.converter.schemas.enable":false,
         "value.converter":"io.confluent.connect.avro.AvroConverter",
